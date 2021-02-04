30ddd2803055bd9ac2bd70595c079dd851b9ea14
christybell <bell.christy27@gmail.com>
2021-02-04 17:58:19 -0600

    completed challenge del 1 and 2

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -25,3 +25,33 @@ ORDER BY rt.emp_no, rt.to_date DESC;
 
 SELECT *
 FROM unique_titles;
+
+-- Create new table for retiring titles
+SELECT COUNT(title), title
+INTO retiring_titles
+FROM unique_titles
+GROUP BY title
+ORDER BY COUNT(title) DESC;
+
+SELECT *
+FROM retiring_titles;
+
+SELECT DISTINCT ON (e.emp_no)
+e.emp_no,
+e.first_name,
+e.last_name,
+e.birth_date,
+de.from_date,
+de.to_date,
+t.title
+INTO mentorship_eligibility
+FROM employees AS e
+JOIN dept_emp AS de
+ON (e.emp_no = de.emp_no)
+JOIN titles AS t
+ON (e.emp_no = t.emp_no)
+WHERE (birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+AND (de.to_date = '9999-01-01');
+
+SELECT *
+FROM mentorship_eligibility;
